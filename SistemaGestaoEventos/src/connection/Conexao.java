dd819b630d8474c5b7ba671527275d555cfddb99
sarahneuburger <61321914+sarahneuburger@users.noreply.github.com>
2021-02-18 19:40:53 -0300

    Ajustando upload anterior

--- SistemaGestaoEventos/src/connection/Conexao.java
+++ SistemaGestaoEventos/src/connection/Conexao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package connection;
 
 import java.sql.Connection;
@@ -39,45 +38,3 @@ public class Conexao {
 	}
 
 }
-=======
-package connection;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class Conexao {
-
-	public static void main(String[] args) {
-
-		Conexao c = new Conexao();
-		c.conectar();
-	}
-
-	public Connection conectar() {
-
-
-		Connection retornoConexao = null;
-
-		try {
-
-			String url = "jdbc:mysql://localhost:3306/gestaoEventos";
-			String usuario = "root";
-			String senha = "123456";
-
-			retornoConexao = DriverManager.getConnection(url, usuario, senha);
-			System.out.println("Conectado com sucesso!");
-
-		} catch (Exception erro) {
-			System.out.println("Falha ao conectar:");
-			System.out.println(erro.getMessage());
-
-			erro.printStackTrace();
-
-		}
-
-		return retornoConexao;
-
-	}
-
-}
->>>>>>> 51895fd3468c1e415dac0dcdfad9f47959b0ea87
--- SistemaGestaoEventos/src/model/EspacoCafeModel.java
+++ SistemaGestaoEventos/src/model/EspacoCafeModel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 public class EspacoCafeModel {
@@ -34,40 +33,3 @@ public class EspacoCafeModel {
 	}
 
 }
-=======
-package model;
-
-public class EspacoCafeModel {
-	
-	private int idEspacoCafe;
-	
-	private String nomeEspacoCafe;
-
-	public int getIdEspacoCafe() {
-		return idEspacoCafe;
-	}
-
-	public void setIdEspacoCafe(int idEspacoCafe) {
-		this.idEspacoCafe = idEspacoCafe;
-	}
-
-	public String getNomeEspacoCafe() {
-		return nomeEspacoCafe;
-	}
-
-	public void setNomeEspacoCafe(String nomeEspacoCafe) {
-		this.nomeEspacoCafe = nomeEspacoCafe;
-	}
-
-	public EspacoCafeModel(int idEspacoCafe, String nomeEspacoCafe) {
-		super();
-		this.idEspacoCafe = idEspacoCafe;
-		this.nomeEspacoCafe = nomeEspacoCafe;
-	}
-	
-	public EspacoCafeModel() {
-		
-	}
-
-}
->>>>>>> 51895fd3468c1e415dac0dcdfad9f47959b0ea87
